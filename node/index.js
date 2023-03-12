a15dec56cb874c5daf7002f3b3bc9fdd0a282d36
Michael Douglas <douglas.ti.dev@gmail.com>
2023-03-12 18:42:17 -0300

    app node gravando no mysql

--- node/index.js
+++ node/index.js
@@ -1,7 +1,16 @@
-import express from "express";
+const express = require("express");
 
 const app = express();
 const port = 3000;
+
+const config = {
+  host: "db",
+  user: "root",
+  password: "root",
+  database: "nodedb",
+};
+const mysql = require("mysql");
+const connection = mysql.createConnection(config);
 
 app.get("/", (req, res) => {
   res.send(
@@ -12,3 +21,7 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log("listening in port " + port);
 });
+
+const sql = `INSERT INTO people(name) values("MICHAEL")`;
+connection.query(sql);
+connection.end();
